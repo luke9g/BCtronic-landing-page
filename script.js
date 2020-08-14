eeb84ced8999ede8a962613838846ea821d7bc5a
Łukasz <lukas.vision@gmail.com>
2020-08-14 21:16:27 +0200

    Landing page - initial version

---
+++ script.js
@@ -0,0 +1,78 @@
+// Hamburger menu ------------------------------------------------------------------------------------------------------
+const hamburgerMenu = document.querySelector(".hamburger");
+
+function toggleHamburger(event) {
+    // Toggle hamburger--active class
+    hamburgerMenu.classList.toggle("hamburger--active");
+
+    // Toggle aria-expanded attribute
+    const currentAriaExpanded = hamburgerMenu.getAttribute("aria-expanded")
+    if (currentAriaExpanded == "false") {
+        newAriaExpanded = "true"
+    } else {
+        newAriaExpanded = "false"
+    }
+    hamburgerMenu.setAttribute("aria-expanded", newAriaExpanded)
+}
+hamburgerMenu.addEventListener("click", toggleHamburger);
+
+
+// Prevent adding focus styles when clicked with a mouse
+let mouseDown = false;
+hamburgerMenu.addEventListener("mousedown", function() {
+    mouseDown = true
+    // blur() in case of already being in focus
+    // hamburgerMenu.blur();
+});
+hamburgerMenu.addEventListener("focus", function() {
+    if (mouseDown) {
+        hamburgerMenu.blur();     
+    }
+    // mouseDown = false
+});
+hamburgerMenu.addEventListener("blur", function() {mouseDown = false});
+
+
+// Close hamburger menu when navigation link is clicked 
+const menuLinks = document.querySelectorAll(".menu-link")
+menuLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+        toggleHamburger(event);
+    })
+})
+
+
+// Toggle sticky header on scroll --------------------------------------------------------------------------------------
+const headerLogo = document.querySelector(".header-logo")
+const header = document.querySelector(".header")
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function toggleStickyHeader(scroll_pos) {
+    if (scroll_pos > 250) {
+        headerLogo.setAttribute("src", "./logo-cut.png")
+        header.classList.add("sticky-header")
+    } else {
+        headerLogo.setAttribute("src", "./logo.png")
+        header.classList.remove("sticky-header")
+    }
+}
+
+// Scroll event throttling (https://developer.mozilla.org/pl/docs/Web/API/Document/scroll_event)
+window.addEventListener('scroll', function() {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+        toggleStickyHeader(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+
+
+// IntersectionObserver ----
